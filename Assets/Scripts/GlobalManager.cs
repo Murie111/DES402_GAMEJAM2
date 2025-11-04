d75dd55bca28669951ee2628e46352362d63639f
BlobyOllie <81163211+BlobyOllie@users.noreply.github.com>
2025-11-04 14:55:43 +0000

    stuff idk anymore

--- Assets/Scripts/GlobalManager.cs
+++ Assets/Scripts/GlobalManager.cs
@@ -18,17 +18,57 @@ public class GlobalManager : MonoBehaviour
     public int fishPoints;
     public int fishermanPoints;
 
+    public GameObject fishTutorial;
+    public GameObject fishermanTutorial;
+
+    public bool fishReady;
+    public bool fishermanReady;
+
+    public GameObject readyText;
+    public GameObject text3;
+    public GameObject text2;
+    public GameObject text1;
+    public GameObject goText;
+
     private void Start()
     {
-        timer = startTime;
+      //  fishTutorial.SetActive(true);
+      //  fishermanTutorial.SetActive(true);
+
         Debug.Log("displays connected: " + Display.displays.Length);
-        //if (Display.displays.Length > 1)
-        //    Display.displays[1].Activate();
-        //else
-        //    Debug.Log("Miss");
+        if (Display.displays.Length > 1)
+        {
+            Display.displays[1].Activate();
+            Display.displays[1].SetParams(1024,512, 2, 1);
+            Screen.SetResolution(1024, 512, true);
+            Application.targetFrameRate = 60;
+        }
+
+        else
+            Debug.Log("Miss");
+    }
+
+    void startCountdown()
+    {
+        Invoke("readyDisp", 0f);
+        Invoke("text3Disp", 1f);
+        Invoke("text2Disp", 2f);
+        Invoke("text1Disp", 3f);
+        Invoke("goDisp", 4f);
+    }
+    void startGame()
+    {
+        timer = startTime;
     }
     void Update()
     {
+        if (fishReady && fishermanReady)
+        {
+            //startCountdown();
+        }    
+
+
+
         if (timer > 0.0f)
         {
             timer -= Time.deltaTime;
@@ -59,5 +99,30 @@ public class GlobalManager : MonoBehaviour
         fishermanPoints += fishermanPointsIncrease;
         fishermanPointsText.text = ("" + fishermanPoints);
         fishermanPointsTextOpp.text = ("" + fishermanPoints);
+    }
+
+    void readyDisp()
+    {
+        readyText.SetActive(true);
+    }
+    void text3Disp()
+    {
+        readyText.SetActive(false);
+        text3.SetActive(true);
+    }
+    void text2Disp()
+    {
+        text3.SetActive(false);
+        text2.SetActive(true);
+    }
+    void text1Disp()
+    {
+        text2.SetActive(false);
+        text1.SetActive(true);
+    }
+    void goDisp()
+    {
+        text1.SetActive(false);
+        goText.SetActive(true);
     }
 }
